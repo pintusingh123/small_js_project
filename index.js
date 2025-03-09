86090c5b68a16a6bcf511c87be68427c36e9217f
pintusingh123 <pintugit982@gmail.com>
2025-03-09 20:19:27 +0530

    comment add in our code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let arr = [];
         
+// data store karne ke liye 
 function storeArray() {
     let input = document.getElementById("arrayInput").value;
+    // condition check krne ke liye input value empty na ho isliye 
     if (input.length >0) {
       arr = input.split(" ");
     updateArrayDisplay();
@@ -9,18 +11,19 @@ function storeArray() {
       alert("please insert element in array ")
     }
 }
-
+ // array update karne ke liye according to user requirement 
 function updateArrayDisplay(highlightIndex = -1) {
     let myArray = document.getElementById('inputArray');
     myArray.innerHTML = "Stored Array: " + arr.map((ele, index) => {
         return `<span class="${index === highlightIndex ? 'bg-yellow-300 px-2' : ''}">${ele}</span>`;
     }).join(", ");
 }
-
+// index number ka use karke array ele find karne ke liye 
 function findEle() {
-    let inputValue = parseInt(document.getElementById('element').value);
-    let result = document.getElementById('result');
-    
+    let inputValue = parseInt(document.getElementById('element').value); //select input value 
+    let result = document.getElementById('result'); //for print array finding ele using index 
+
+     //condition add ki hai agr input value number nhi h to 
     if (isNaN(inputValue)) {
         result.innerText = "Please enter a valid number!";
         result.classList.add("text-red-500");
